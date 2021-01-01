ebe0eade014246f36dd574a255f48bededb5bef2
ThisUsernameIsCool <60085040+ThisUsernameIsCool@users.noreply.github.com>
2021-01-01 04:45:31 -0500

    Create Start.cpp

---
+++ ex/Start.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+  std::cout << "Are you sure you want to start?" << std::endl;
+  std::string answer;
+  std::cin >> answer;
+
+  if (answer == "y" || answer == "yes") {
+    if (system("start ./ex/Main.py") != 0) {
+      MessageBox(NULL, "Could not find ./ex/Main.py\nError Code: 0x00000001", "Exception!", NULL);
+    }
+    return 0;
+  } else {
+    return -1;
+  }
+}
