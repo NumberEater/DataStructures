9ae189738b8cb53924f2d964f730bb4a55690b02
ThisUsernameIsCool <60085040+ThisUsernameIsCool@users.noreply.github.com>
2021-01-01 20:27:38 -0500

    Update Start.cpp

--- ex/Start.cpp
+++ ex/Start.cpp
@@ -4,10 +4,10 @@
 int main()
 {
   std::cout << "Are you sure you want to start?" << std::endl;
-  std::string answer;
+  char answer;
   std::cin >> answer;
 
-  if (answer == "y" || answer == "yes") {
+  if (answer == 'y') {
     if (system("start ./ex/Main.py") != 0) {
       MessageBox(NULL, "Could not find ./ex/Main.py\nError Code: 0x00000001", "Exception!", NULL);
     }
