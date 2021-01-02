b129755349bb2590f8a9114449b2f6cdfa6d57a6
ThisUsernameIsCool <60085040+ThisUsernameIsCool@users.noreply.github.com>
2021-01-01 20:43:09 -0500

    Update Main.py

--- ex/Main.py
+++ ex/Main.py
@@ -4,19 +4,35 @@ def pause():
     print()
     system("pause")
 
-example = {"my_car":
+def colored(r, g, b, text):
+    system("color")
+    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
+family_cars = {"my_car":
         {"brand": "Ford",
         "model": "Mustang",
-        "year": 1964
+        "year": 1994
         },
         "mom_car":
-        {"brand": "Ford",
-        "model": "Fusion",
+        {"brand": "Chevy",
+        "model": "Traverse",
+        "year": 2016
+        },
+        "sister_car":
+        {"brand": "Chevy",
+        "model": "HHR",
         "year": 2011
+        },
+        "dad_car":
+        {"brand": "Tesla",
+        "model": "Model X",
+        "year": 2016
         }
 }
 
-print(example["my_car"]["brand"], example["my_car"]["model"])
-print(example["mom_car"]["brand"], example["mom_car"]["model"])
+print(colored(255, 0, 0, "My car:") + family_cars["my_car"]["brand"], family_cars["my_car"]["model"] + "\n")
+print(colored(255, 255, 0, "My mom's car:") + family_cars["mom_car"]["brand"], family_cars["mom_car"]["model"] + "\n")
+print(colored(255, 0, 153, "My sister's car:") + family_cars["sister_car"]["brand"], family_cars["sister_car"]["model"] + "\n")
+print(colored(0, 0, 255, "My dad's car:") + family_cars["dad_car"]["brand"], family_cars["dad_car"]["model"] + "\n")
 
 pause()
